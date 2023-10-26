ea34700ed95b90c6aa19186b4c33c86551e8798a
Evgenii Gutin <56745707+YUNGC0DE@users.noreply.github.com>
2023-10-26 22:49:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         for r in pkg_resources.parse_requirements(
             open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
         )
-    ] + ["pyannote.audio @ git+https://github.com/pyannote/pyannote-audio@ad8a9e9d405343c5a93ccc11b641d0a2e8842a0d"],
+    ] + ["pyannote.audio @ git+https://github.com/YUNGC0DE/pyannote-audio.git"],
     entry_points = {
         'console_scripts': ['whisperx=whisperx.transcribe:cli'],
     },
